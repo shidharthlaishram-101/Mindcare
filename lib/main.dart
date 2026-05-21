9b3cdd7338b36c0a046bd60dedabf654f7238185
shwwid <shidharthlaishram@gmail.com>
2026-05-22 03:06:47 +0530

    flutterfire configured for iOS, flutter options.dart was generated and firebase.json got generated which are important

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mindcare/start.dart';
+import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
