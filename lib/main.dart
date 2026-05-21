d73103e1277b0aa015f118ca3114f4a242becbe4
shwwid <shidharthlaishram@gmail.com>
2026-05-22 03:00:32 +0530

    pubspec is updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
